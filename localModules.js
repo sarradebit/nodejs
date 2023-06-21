1a65f6e63658410244b9d3a37a6003d2a6e64d35
sarradebit <Sarahdebit4@gmail.com>
2023-06-21 18:39:10 +0100

    nodejs

---
+++ localModules.js
@@ -0,0 +1,6 @@
+const sum = (a,b) => a+b 
+
+
+const minus = (a,b) => a-b
+
+modules.exports = {sum , minus}
---
+++ server.js
@@ -0,0 +1,9 @@
+//const sum = (a,b) => a+b ;
+const calcul  = require("./localModules")
+ 
+let a = 2
+let b = 5
+
+
+
+console.log(`the sum of ${a} and ${b} is`  , calcul.sum(a,b) )   
